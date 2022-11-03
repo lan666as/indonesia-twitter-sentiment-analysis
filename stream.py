997fa6fbd7126de837ac6f80a50ad0856a8872e8
Ahmad Zidan <4zidan@gmail.com>
2022-11-03 14:13:38 +0700

    fix stream.py

--- stream.py
+++ stream.py
@@ -23,30 +23,45 @@ class TwitterStreamingClient(tweepy.StreamingClient):
         
         data = response.data
         matching_rules = response.matching_rules
+        if "users" in response.includes:
+            users_dict = {user.id:user for user in response.includes["users"]}
+        if "places" in response.includes:
+            places_dict = {place.id:place for place in response.includes["places"]}
+        if "tweets" in response.includes:
+            tweets_dict = {tweet.id:tweet for tweet in response.includes["tweets"]}
 
         if data is not None:
-            id = data.id
-            date = data.created_at.isoformat()
-            text = data.text
-            tags = [matching_rule.tag.split(';') for matching_rule in matching_rules] 
+            tags = [matching_rule.tag.split(';') for matching_rule in matching_rules]
+            
+            lat, long = None, None
+            if data.geo:
+                lat1, long1, lat2, long2 = places_dict[data.geo["place_id"]].geo["bbox"]
+                lat, long = (lat1+lat2)/2, (long1+long2)/2
             
             message = {
-                "id" : id,
-                "date" : date,
-                "text" : text,
+                "id" : data.id,
+                "author_username" : users_dict[data.author_id].username,
+                "created_at" : data.created_at.isoformat(),
+                "text" : data.text,
+                "lat" : lat, "long" : long,
+                "source" : data.source,
+                "retweet_count" : data.public_metrics["retweet_count"],
+                "reply_count" : data.public_metrics["reply_count"],
+                "like_count" : data.public_metrics["like_count"],
+                "quote_count" : data.public_metrics["quote_count"],
+                "possibly_sensitive" : data.possibly_sensitive,
                 "tags" : [tag[0] for tag in tags],
             }
             
             self.producer.send(tags[0][1], message)
 
             print(message)
-            print("#"*80)
 
-streaming_client_test = TwitterStreamingClient(BEARER_TOKEN)
-print(f"Rule used: {streaming_client_test.get_rules()}")
+streaming_client = TwitterStreamingClient(BEARER_TOKEN)
+print(f"Rule used: {streaming_client.get_rules()}")
 
 
-streaming_client_test.filter(
+streaming_client.filter(
     expansions=ALL_EXPANSIONS,
     media_fields=ALL_MEDIA_FIELDS,
     place_fields=ALL_PLACE_FIELDS,
